b88f19055794735d491d49648498898e4d6b03aa
Tom Hadlaw <tom.hadlaw@isovalent.com>
2022-09-20 20:49:23 -0700

    server: provide API server implementation for map events endpoint.
    Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -1977,6 +1977,7 @@ func (d *Daemon) instantiateAPI() *restapi.CiliumAPIAPI {
 	// /map
 	restAPI.DaemonGetMapHandler = NewGetMapHandler(d)
 	restAPI.DaemonGetMapNameHandler = NewGetMapNameHandler(d)
+	restAPI.DaemonGetMapNameEventsHandler = NewGetMapNameEventsHandler(d, mapGetterImpl{})
 
 	// metrics
 	restAPI.MetricsGetMetricsHandler = NewGetMetricsHandler(d)
