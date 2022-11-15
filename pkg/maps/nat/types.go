86e8961db8a6f3b7b931b71460e8f595ef6a3a78
Tom Hadlaw <tom.hadlaw@isovalent.com>
2022-11-15 21:58:50 +0000

    pkg/maps/nat: refactor nat structs into seperate package
    Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

--- pkg/maps/nat/types.go
+++ pkg/maps/nat/types.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 	"unsafe"
 
+	natTypes "github.com/cilium/cilium/pkg/maps/nat/types"
+
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/tuple"
 	"github.com/cilium/cilium/pkg/u8proto"
@@ -34,9 +36,7 @@ type NatKey interface {
 // NatKey4 is needed to provide NatEntry type to Lookup values
 // +k8s:deepcopy-gen=true
 // +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
-type NatKey4 struct {
-	tuple.TupleKey4Global
-}
+type NatKey4 natTypes.NatKey4
 
 // SizeofNatKey4 is the size of the NatKey4 type in bytes.
 const SizeofNatKey4 = int(unsafe.Sizeof(NatKey4{}))
@@ -76,9 +76,7 @@ func (k *NatKey4) GetNextHeader() u8proto.U8proto {
 // NatKey6 is needed to provide NatEntry type to Lookup values
 // +k8s:deepcopy-gen=true
 // +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
-type NatKey6 struct {
-	tuple.TupleKey6Global
-}
+type NatKey6 natTypes.NatKey6
 
 // SizeofNatKey6 is the size of the NatKey6 type in bytes.
 const SizeofNatKey6 = int(unsafe.Sizeof(NatKey6{}))
