e059a991684a7e7fecc3fb8589463f6198d05443
Tom Hadlaw <tom.hadlaw@isovalent.com>
2023-05-19 22:18:38 -0700

    wip:pkg/errs

---
+++ pkg/errs/errs.go
@@ -0,0 +1,15 @@
+package errs
+
+import (
+	"github.com/sirupsen/logrus"
+	"go.uber.org/multierr"
+)
+
+// Into is a helper around multierr.AppendInto that ensures that it never panics
+// in the case of a nil error pointer.
+func Into(into *error, err error) bool {
+	if into == nil {
+		logrus.Error("[BUG] errs.Into called with nil error pointer")
+	}
+	return multierr.AppendInto(into, err)
+}
