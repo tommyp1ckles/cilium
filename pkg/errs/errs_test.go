f2bdcc1344a6b34e4266ffb91ece0d26a1d406fc
Tom Hadlaw <tom.hadlaw@isovalent.com>
2023-06-13 16:55:32 -0700

    add pkg/errs example
    Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

---
+++ pkg/errs/errs_test.go
@@ -0,0 +1,19 @@
+package errs
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"golang.org/x/sys/unix"
+)
+
+func TestErr(t *testing.T) {
+	// the message also contains the full error.
+	leaf := errors.Join(fmt.Errorf("Failed to create default drop route for ipv4: %w", unix.EINVAL), fmt.Errorf("Failed to create xfrm policy for ipv4: %w", unix.EINTR))
+	// TODO: Managing doc links sounds like a nightmare.
+	//	Need to check these?
+	branch := WithHelp(fmt.Errorf("Enabling IPSec: %w", leaf), "Check error code, likely caused by host issues. Ensure that iproute2 >= v6.6 is installed. See: https://docs.cilium.io/en/latest/security/network/encryption-ipsec/ for details")
+	err := WithHelp(fmt.Errorf("Failed to validate node using linux-datapath: %w", errors.Join(branch, fmt.Errorf("Enabling node encryption: %w", leaf))), "Failed node datapath validation: May be in degraded state")
+	Print(err)
+}
---
+++ pkg/errs/helpful.go
@@ -0,0 +1,120 @@
+package errs
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// This is some example code for how we might approach dealing with errors
+// emitted by Cilium reconciliation code.
+//
+// That is, code that performs procedures that are intended to reconcile "realized"
+// and actual system state.
+//
+// Currently, our error handling is inconsistent, this seeks to improve:
+// * Provide a way of dealing with complex, heirachical, error traces in a easy, and un-univasive way.
+// * Improve error reporting such that it is:
+//		* More meaningful for engineers/users by providing better context on what/where the failure occured.
+//      * Provide more end-user facing error messages that supply potentially additional helpful information, links, data, etc.
+//
+// This library provides some functionality built on top of existing Go error handling patterns, as well as the standard "errors" library
+// 	to help developers write/report errors.
+//
+//
+// Example Error (Print):
+//
+// Failed to validate node using linux-datapath
+//   Enabling IPSec: Check error code, likely caused by host issues. Ensure that iproute2 >= v6.6 is installed. See: https://docs.cilium.io/en/latest/security/network/encryption-ipsec/ for details
+//     Failed to create default drop route for ipv4
+//       invalid argument
+//     Failed to create xfrm policy for ipv4
+//       interrupted system call
+//   Enabling node encryption
+//     Failed to create default drop route for ipv4
+//       invalid argument
+//     Failed to create xfrm policy for ipv4
+//       interrupted system call
+
+// ciliumError implements an error, that adds additional help information.
+// When unrwapped, this behaves like a normal error, the only way to access
+// the help message is by doing (checked!) dynamic type assertions to see if
+// an error has some Cilium specific help info.
+//
+// TODO: Think of a better name?
+type ciliumError struct {
+	helpMessage string
+	err         error
+}
+
+func WithHelp(err error, help string) error {
+	return &ciliumError{helpMessage: help, err: err}
+}
+
+func getHelpMessage(err error) (*string, error) {
+	if ce, ok := err.(*ciliumError); ok {
+		return &ce.helpMessage, ce.err
+	}
+	return nil, err
+}
+
+func (c *ciliumError) Error() string {
+	return c.err.Error()
+}
+
+func (c *ciliumError) Unwrap() error {
+	return c.err
+}
+
+func Unwrap(err error) []error {
+	if errs, ok := err.(interface{ Unwrap() []error }); ok {
+		return errs.Unwrap()
+	} else {
+		// In the case like fmt.Errorf('root: %w', errs) the first
+		// unwrap is just the flat error of errs, because we're interested
+		// in the "children" joined errs we attempt another unwrap to see
+		// if there is a list of underlying errors to this value.
+		err = errors.Unwrap(err)
+		var errs []error
+		if es, ok := err.(interface{ Unwrap() []error }); ok {
+			errs = es.Unwrap()
+		} else {
+			errs = append(errs, err)
+		}
+		return errs
+	}
+}
+
+func Message(err error) string {
+	if child := errors.Unwrap(err); child == nil {
+		// If we're at a leaf error, just return the whole error message.
+		return err.Error()
+	} else {
+		s := strings.ReplaceAll(err.Error(), child.Error(), "")
+		s = strings.TrimRight(s, ": ")
+		return s
+	}
+}
+
+func fmtMsg(s string, indent int) string {
+	return strings.ReplaceAll(s, "\n", "\n"+strings.Repeat(".", indent))
+}
+
+func Print(err error) {
+	printErr(0, err)
+}
+
+func printErr(indent int, err error) {
+	if err == nil {
+		return
+	}
+	if help, err2 := getHelpMessage(err); help != nil {
+		fmt.Printf("%s%s: %s\n", strings.Repeat(" ", indent), fmtMsg(Message(err2), indent), *help)
+		err = err2
+	} else {
+		fmt.Printf("%s%s\n", strings.Repeat(" ", indent), fmtMsg(Message(err), indent))
+	}
+	for _, err := range Unwrap(err) {
+		printErr(indent+2, err)
+	}
+}
