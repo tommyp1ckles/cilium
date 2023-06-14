ba5fb2a855e24938291d6f9c70a374e734115883
Tom Hadlaw <tom.hadlaw@isovalent.com>
2023-06-14 13:55:44 -0700

    test core error semantics when using help.
    Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

--- pkg/errs/errs_test.go
+++ pkg/errs/errs_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/sys/unix"
 )
 
@@ -16,4 +17,12 @@ func TestErr(t *testing.T) {
 	branch := WithHelp(fmt.Errorf("Enabling IPSec: %w", leaf), "Check error code, likely caused by host issues. Ensure that iproute2 >= v6.6 is installed. See: https://docs.cilium.io/en/latest/security/network/encryption-ipsec/ for details")
 	err := WithHelp(fmt.Errorf("Failed to validate node using linux-datapath: %w", errors.Join(branch, fmt.Errorf("Enabling node encryption: %w", leaf))), "Failed node datapath validation: May be in degraded state")
 	Print(err)
+}
+
+func TestErrSemantics(t *testing.T) {
+	e1 := errors.New("1")
+	err := WithHelp(fmt.Errorf("foo: %w", e1), "Some msg!")
+	assert.ErrorIs(t, err, e1)
+	err = fmt.Errorf("root: %w", errors.Join(fmt.Errorf("?"), WithHelp(fmt.Errorf("zzz: %w", e1), "???")))
+	assert.ErrorIs(t, err, e1)
 }
